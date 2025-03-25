13fb3833019de93b96314d46589d5e9cab6ee2e5
Gaubongtoi <tanvaquy123@gmail.com>
2025-03-26 02:19:41 +0700

    third commit

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -230,35 +230,35 @@ function HomePage() {
                     </>
                 }
             >
-                <div class="bg-primary_black text-white rounded-[45px] p-16 flex justify-between border-2 mt-10 gap-10 text-xl">
-                    <div class="w-1/3">
+                <div className="bg-primary_black text-white rounded-[45px] p-16 flex justify-between border-2 mt-10 gap-10 text-xl">
+                    <div className="w-1/3">
                         <p>
                             For a local restaurant, we implemented a targeted PPC campaign that resulted in a 50%
                             increase in website traffic and a 25% increase in sales.
                         </p>
-                        <a href="#" class="text-primary_green flex items-center gap-2 mt-4">
+                        <a href="#" className="text-primary_green flex items-center gap-2 mt-4">
                             Learn more <img src={icons.LearnMoreArrowUp} alt="" />
                         </a>
                     </div>
-                    <div class="w-px bg-gray-500"></div>
-                    <div class="w-1/3 relative">
+                    <div className="w-px bg-gray-500"></div>
+                    <div className="w-1/3 relative">
                         <p>
                             For a B2B software company, we developed an SEO strategy that resulted in a first page
                             ranking for key keywords and a 200% increase in organic traffic.
                         </p>
-                        <a href="#" class="text-primary_green flex items-center gap-2 mt-4">
+                        <a href="#" className="text-primary_green flex items-center gap-2 mt-4">
                             Learn more <img src={icons.LearnMoreArrowUp} alt="" />
                         </a>
                     </div>
 
-                    <div class="w-px bg-gray-500"></div>
+                    <div className="w-px bg-gray-500"></div>
 
-                    <div class="w-1/3">
+                    <div className="w-1/3">
                         <p>
                             For a national retail chain, we created a social media marketing campaign that increased
                             followers by 25% and generated a 20% increase in online sales.
                         </p>
-                        <a href="#" class="text-primary_green flex items-center gap-2 mt-4">
+                        <a href="#" className="text-primary_green flex items-center gap-2 mt-4">
                             Learn more <img src={icons.LearnMoreArrowUp} alt="" />
                         </a>
                     </div>
